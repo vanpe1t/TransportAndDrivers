0ef207452364beefef850dc4d855cb12a83fe9e2
vanpe1t <107750861+vanpe1t@users.noreply.github.com>
2023-06-23 09:52:10 +0300

    Initial commit

---
+++ src/main/java/com/example/transportanddrivers/models/Transport.java
@@ -0,0 +1,14 @@
+package com.example.transportanddrivers.models;
+
+public abstract class Transport {
+
+    private int weight;
+    private int maxSpeed;
+    private String color;
+
+
+    public abstract void gas();
+
+    public abstract void break();
+
+}
---
+++ src/test/java/com/example/transportanddrivers/TransportAndDriversApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transportanddrivers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportAndDriversApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
